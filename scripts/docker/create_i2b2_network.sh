824fe375ff3192ea30c131997ca4a3f641ca0313
Cloud User <centos@ip-172-31-36-188.ec2.internal>
2018-03-03 01:35:16 +0000

    tagging images to local tags and fixing pg errors

--- scripts/docker/create_i2b2_network.sh
+++ scripts/docker/create_i2b2_network.sh
@@ -5,6 +5,8 @@ DOCKER_HOME=$BASE/local/docker
 DIP=$2
 alias docker=" docker"
 
+echo "DIP:$DIP"
+
 docker network create i2b2-net
 
 echo "PWD;$(pwd)"
@@ -44,8 +46,8 @@ else
 
 
 	docker stop $APP;docker rm $APP; docker rmi i2b2/$APP
-	docker build  -t i2b2/i2b2-wildfly $DAP/
-	docker run -d -p 8080:8080 --net i2b2-net --name $APP i2b2/i2b2-wildfly
+	docker build  -t local/i2b2-wildfly $DAP/
+	docker run -d -p 8080:8080 --net i2b2-net --name $APP local/i2b2-wildfly
 	
 
 fi
@@ -59,17 +61,18 @@ else
 	mkdir -p "$DAP" && echo "created $DAP"
 	echo "BASE:$BASE DIP:$DIP DAP:$DAP"
 	source $BASE/scripts/install/centos_sudo_install.sh $BASE
+	DIP=$2
 	copy_webclient_dir $BASE $DIP $DAP
 
 	cp -rv $BASE/conf/httpd/* $DAP
 	cp -rv $BASE/conf/docker/$APP/* $DAP
-	sed -i  s/9090/8080/ $DAP/i2b2_proxy.conf
+#	sed -i  s/9090/8080/ $DAP/i2b2_proxy.conf
 #	sed -i  s/localhost/$DIP/ $DAP/i2b2_proxy.conf
-	sed -i  s/localhost/i2b2-wildfly/ $DAP/i2b2_proxy.conf
+#	sed -i  s/localhost/i2b2-wildfly/ $DAP/i2b2_proxy.conf
 
 	docker stop $APP;docker rm $APP; docker rmi i2b2/$APP
-	docker build  -t i2b2/i2b2-web $DAP/
-	docker run -d  -p 443:443 -p 80:80 --net i2b2-net --name i2b2-web i2b2/i2b2-web /run-httpd.sh localhost
+	docker build  -t local/i2b2-web $DAP/
+	docker run -d  -p 443:443 -p 80:80 --net i2b2-net -e APP_IP="$DIP" --name i2b2-web local/i2b2-web /run-httpd.sh 
 fi
 
 
@@ -89,8 +92,8 @@ else
 	
 	cp -rv $BASE/conf/docker/$APP/* $DAP
 	
-	docker build  -t i2b2/i2b2-pg $DAP/
-	docker run -d  -p 5432:5432 --net i2b2-net --name i2b2-pg   -e 'DB_USER=i2b2' -e 'DB_PASS=pass' -e 'DB_NAME=i2b2' i2b2/i2b2-pg
+	docker build  -t local/i2b2-pg $DAP/
+	docker run -d  -p 5432:5432 --net i2b2-net --name i2b2-pg   -e 'DB_USER=i2b2' -e 'DB_PASS=pass' -e 'DB_NAME=i2b2' local/i2b2-pg
 
 	source $BASE/scripts/postgres/load_data.sh $(pwd) 
 
@@ -107,7 +110,7 @@ else
 	#sleep 5;
 	#docker tag i2b2
 	CID=$(docker ps -aqf "name=i2b2-empty")
-	docker commit $CID i2b2/i2b2-pg
+	docker commit $CID local/i2b2-pg
 	docker exec -it i2b2-pg bash -c "export PUBLIC_IP=$IP_ADD;sh update_pm_cell_data.sh; "
 
 
