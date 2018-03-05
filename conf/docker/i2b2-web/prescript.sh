ff919315eeaab9d4f7fafca6b26bbaaaa5dc766a
Cloud User <centos@ip-172-31-41-164.ec2.internal>
2018-03-05 21:36:46 +0000

    docker works even by blocking port 8080

--- conf/docker/i2b2-web/prescript.sh
+++ conf/docker/i2b2-web/prescript.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+printenv
 echo "running prescript with arg:$1"
 #sed  -e "s/localhost/$1/" /conftemp/var/www/html/webclient/i2b2_config_data.js > /var/www/html/webclient/i2b2_config_data.js
 #sed  -e "s/localhost/$1/" /conftemp/var/www/html/admin/i2b2_config_data.js > /var/www/html/admin/i2b2_config_data.js
-sed  -e "s/i2b2-wildfly/$1/" /conftemp/etc/httpd/conf.d/i2b2_proxy.conf |sed -e s/9090/8080/ > /etc/httpd/conf.d/i2b2_proxy.conf
+#sed  -e "s/i2b2-wildfly/$1/" /conftemp/etc/httpd/conf.d/i2b2_proxy.conf |sed -e s/9090/8080/ > /etc/httpd/conf.d/i2b2_proxy.conf
+cat /conftemp/etc/httpd/conf.d/i2b2_proxy.conf |sed -e s/9090/8080/ > /etc/httpd/conf.d/i2b2_proxy.conf
