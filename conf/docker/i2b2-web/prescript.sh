56e717ef12b568189eb957069ec033091ad63c4a
Cloud User <centos@ip-172-31-41-164.ec2.internal>
2018-03-05 21:44:46 +0000

    to allow ssl connections to webclient

--- conf/docker/i2b2-web/prescript.sh
+++ conf/docker/i2b2-web/prescript.sh
@@ -5,3 +5,4 @@ echo "running prescript with arg:$1"
 #sed  -e "s/localhost/$1/" /conftemp/var/www/html/admin/i2b2_config_data.js > /var/www/html/admin/i2b2_config_data.js
 #sed  -e "s/i2b2-wildfly/$1/" /conftemp/etc/httpd/conf.d/i2b2_proxy.conf |sed -e s/9090/8080/ > /etc/httpd/conf.d/i2b2_proxy.conf
 cat /conftemp/etc/httpd/conf.d/i2b2_proxy.conf |sed -e s/9090/8080/ > /etc/httpd/conf.d/i2b2_proxy.conf
+cat /conftemp/etc/httpd/conf.d/ssl.conf |sed -e s/9090/8080/ > /etc/httpd/conf.d/ssl.conf
