834aee114b7c14b561ffa094b405789860b9a56a
waghsk@gmail.com <centos@ip-172-31-32-97.ec2.internal>
2018-03-08 23:04:09 +0000

    to fix i2b2 wildfly services

--- conf/docker/i2b2-pg/pm_access_insert_data.sql
+++ conf/docker/i2b2-pg/pm_access_insert_data.sql
@@ -1,11 +1,11 @@
 INSERT INTO PM_CELL_DATA (CELL_ID, PROJECT_PATH, NAME, METHOD_CD, URL, CAN_OVERRIDE, STATUS_CD)
-  VALUES('CRC', '/', 'Data Repository', 'REST', 'http://localhost:9090/i2b2/services/QueryToolService/', 1, 'A');
+  VALUES('CRC', '/', 'Data Repository', 'REST', 'http://localhost/i2b2/services/QueryToolService/', 1, 'A');
 INSERT INTO PM_CELL_DATA(CELL_ID, PROJECT_PATH, NAME, METHOD_CD, URL, CAN_OVERRIDE, STATUS_CD)
-  VALUES('FRC', '/', 'File Repository ', 'SOAP', 'http://localhost:9090/i2b2/services/FRService/', 1, 'A');
+  VALUES('FRC', '/', 'File Repository ', 'SOAP', 'http://localhost/i2b2/services/FRService/', 1, 'A');
 INSERT INTO PM_CELL_DATA(CELL_ID, PROJECT_PATH, NAME, METHOD_CD, URL, CAN_OVERRIDE, STATUS_CD)
-  VALUES('ONT', '/', 'Ontology Cell', 'REST', 'http://localhost:9090/i2b2/services/OntologyService/', 1, 'A');
+  VALUES('ONT', '/', 'Ontology Cell', 'REST', 'http://localhost/i2b2/services/OntologyService/', 1, 'A');
 INSERT INTO PM_CELL_DATA(CELL_ID, PROJECT_PATH, NAME, METHOD_CD, URL, CAN_OVERRIDE, STATUS_CD)
-  VALUES('WORK', '/', 'Workplace Cell', 'REST', 'http://localhost:9090/i2b2/services/WorkplaceService/', 1, 'A');
+  VALUES('WORK', '/', 'Workplace Cell', 'REST', 'http://localhost/i2b2/services/WorkplaceService/', 1, 'A');
 INSERT INTO PM_CELL_DATA(CELL_ID, PROJECT_PATH, NAME, METHOD_CD, URL, CAN_OVERRIDE, STATUS_CD)
-  VALUES('IM', '/', 'IM Cell', 'REST', 'http://localhost:9090/i2b2/services/IMService/', 1, 'A');
+  VALUES('IM', '/', 'IM Cell', 'REST', 'http://localhost/i2b2/services/IMService/', 1, 'A');
 
